9ac54506f3c5b2532960487f2a97ac9d1603f9c7
lukehart54 <lukehart54@users.noreply.github.com>
2022-02-23 22:30:24 -0500

    Added algo and test

--- src/word-util.test.ts
+++ src/word-util.test.ts
@@ -1,9 +1,50 @@
-import {describe, expect, it} from 'vitest';
-import { getRandomWord } from './word-utils';
-
+import { describe, expect, it } from 'vitest';
+import { computeGuess, getRandomWord, LetterState } from './word-utils';
 
 describe('word-utils', () => {
-    it('random word', () => {
-        expect(getRandomWord()).toEqual('sugar');
-    });
+  it('random word', () => {
+    expect(getRandomWord().length).toEqual(5);
+  });
+});
+
+describe('computeGuess', () => {
+  it('it works with match and present', () => {
+    expect(computeGuess('boost', 'basic')).toEqual([
+      LetterState.Match,
+      LetterState.Wrong,
+      LetterState.Wrong,
+      LetterState.Present,
+      LetterState.Wrong,
+    ]);
+  });
+
+  it('it works with all matches', () => {
+    expect(computeGuess('boost', 'boost')).toEqual([
+      LetterState.Match,
+      LetterState.Match,
+      LetterState.Match,
+      LetterState.Match,
+      LetterState.Match,
+    ]);
+  });
+
+  it('it works with all matches', () => {
+    expect(computeGuess('guard', 'boost')).toEqual([
+      LetterState.Wrong,
+      LetterState.Wrong,
+      LetterState.Wrong,
+      LetterState.Wrong,
+      LetterState.Wrong,
+    ]);
+  });
+
+  it('it works with all matches', () => {
+    expect(computeGuess('solid', 'boost')).toEqual([
+      LetterState.Present,
+      LetterState.Match,
+      LetterState.Wrong,
+      LetterState.Wrong,
+      LetterState.Wrong,
+    ]);
+  });
 });
--- src/word-utils.ts
+++ src/word-utils.ts
@@ -4,3 +4,27 @@ export function getRandomWord() {
     const randomIndex = Math.floor(Math.random() * wordBank.length);
     return wordBank[randomIndex];
 }
+
+export enum LetterState {
+    Wrong,
+    Present,
+    Match
+}
+
+export function computeGuess(guess: string, wordString: string) {
+    const guessArray = guess.split('');
+    const answerArray = wordString.split('');
+
+    const result: LetterState[] = [];
+    guessArray.forEach((letter, index) => {
+        if (letter === answerArray[index]) {
+            result.push(LetterState.Match);
+        } else if (answerArray.includes(letter)) {
+            result.push(LetterState.Present);
+        } else {
+            result.push(LetterState.Wrong);
+        }
+    })
+
+    return result;
+}
